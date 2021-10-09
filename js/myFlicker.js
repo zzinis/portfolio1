08424b75a9448a578288afdd34f158a20ba43899
zzinis <lizzyin@naver.com>
2021-10-09 19:20:46 +0900

    gallery 레이아웃 수정

--- js/myFlicker.js
+++ js/myFlicker.js
@@ -3,11 +3,11 @@ $.ajax({
     dataType:"json", 
     data:{
         api_key:"fce3d5dec157a9b2bb0797070e4fd0b8", 
-        per_page:20, 
+        per_page:8, 
         format:"json",
         nojsoncallback:1, 
         privacy_filter : 5, 
-        tags :"landscape" 
+        tags :"cloud" 
     }
 })
 .success(function(data){
