221dbfbcdfbf240c1efdef2c564b821d8b22d941
zzinis <lizzyin@naver.com>
2021-10-12 17:54:03 +0900

    location contact 추가

--- js/map.js
+++ js/map.js
@@ -4,5 +4,4 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         level: 3 // 지도의 확대 레벨
     };
 
-// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption); 
