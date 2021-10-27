8a04104314696f637468cab09cceb41f3da7b5ea
zzinis <lizzyin@naver.com>
2021-10-27 14:02:47 +0900

    join email 추가

--- js/join.js
+++ js/join.js
@@ -3,6 +3,7 @@ $("input[type=submit]").on("click", function(e){
     if(!isTxt("userid", 5)) e.preventDefault(); 
     if(!isName("username")) e.preventDefault(); 
     if(!isPwd("pwd1", "pwd2", 5)) e.preventDefault(); 
+    if(!isEmail("email")) e.preventDefault();
 }); 
 
 function isTxt(name, len){
@@ -59,6 +60,22 @@ function isPwd(name1, name2, len){
         $("input[name="+name1+"]").parent().find("span").remove(); 
         $("input[name="+name1+"]").parent().append(
             "<span>영문과 숫자는 "+len+"글자 이상을 사용해야 하며 특수문자, 숫자를 포함해주세요.</span>"
+        ); 
+        return false; 
+    }
+}
+
+function isEmail(name) {
+
+    let txt = $("[name="+name+"]").val(); 
+
+    if(email == ""){      
+        $("[name="+name+"]").parent().find("span").remove(); 
+        return true; 
+    }else{      
+        $("[name="+name+"]").parent().find("span").remove(); 
+        $("[name="+name+"]").parent().append(
+            "<span>메일 아이디를 입력하세요.</span>"
         ); 
         return false; 
     }
