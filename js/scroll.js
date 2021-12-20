efb8e13e6a2b9cbcd7362894a084c90627224497
zzinis <lizzyin@naver.com>
2021-12-20 13:35:21 +0900

    main scroll js 변경

--- js/scroll.js
+++ js/scroll.js
@@ -1,47 +1,79 @@
-const $btns = $("#navi li"); 
-const $boxs = $(".myScroll"); 
-let posArr = []; 
-let len = $btns.length; 
-let baseLine = -300; 
+const sections = document.querySelectorAll(".myScroll");
+const lis = document.querySelectorAll("#navi li");
+const lis_arr = Array.from(lis);
+const len = sections.length;
+const speed = 500;
+let posArr = [];
+let base = -200;
 
-
-for(let i=0; i<len; i++){
-    posArr.push($boxs.eq(i).offset().top);     
-}
-
-$(window).on("resize", function(){
-    posArr = []; 
-    for(let i=0; i<len; i++){
-        posArr.push($boxs.eq(i).offset().top);     
+setPos();
+window.addEventListener("resize", ()=>{
+    setPos();
+    let activeItem = document.querySelector("ul li.on");
+    let activeInedx = lis_arr.indexOf(activeItem); 
+ 
+    //모션없이 해당위치로 바로 이동
+    window.scroll(0, posArr[activeInedx]);
+ })
+ 
+ window.addEventListener("mousewheel", e=>{
+    e.preventDefault();
+ 
+    let activeItem = document.querySelector("ul li.on");
+    let activeIndex = lis_arr.indexOf(activeItem);
+    let targetIndex;
+ 
+    //마우스휠을 올리면(-100)
+    if(e.deltaY < 0){
+       if(activeIndex == 0) return;
+       targetIndex = activeIndex -1;
+    }else{
+       if(activeIndex == len-1) return;
+       //마우스휠을 내리면(100)
+       targetIndex = activeIndex +1;
     }
+ 
+    new Anim(window, {
+       prop : "scroll",
+       value : posArr[targetIndex],
+       duration : speed
+    })
+ }, {passive : false})
+ 
+ 
 
-    console.log(posArr); 
-});
+window.addEventListener("scroll", e=>{
+    let scroll = window.scrollY || window.pageYOffset;
+    sections.forEach((el,index)=>{
+        if(scroll >= posArr[index] + base){
+            lis.forEach((el,i)=>{
+                el.classList.remove("on");
+                sections[i].classList.remove("on");
+            })
 
-
-$(window).on("scroll", function(){
-    var scroll = $(this).scrollTop(); 
-    for(let i =0; i<len; i++){
-        if(scroll >= posArr[i] + baseLine){
-            $btns.children("a").removeClass("on"); 
-            $btns.eq(i).children("a").addClass("on"); 
-
+            lis[index].classList.add("on");
+            sections[index].classList.add("on");
         }
+    })
+})
+
+lis.forEach((el,index)=>{
+    el.addEventListener("click", e=>{
+        new Anim(window, {
+            prop:"scroll",
+            value:posArr[index],
+            duration:500
+        })
+        for(let el of lis){
+            el.classList.remove("on");
+        }
+        e.currentTarget.classList.add("on");
+    })
+})
+
+function setPos(){
+    posArr = [];
+    for(let el of sections){
+        posArr.push(el.offsetTop);
     }
-}); 
-
-$("#navi li a").on("click", function(e){
-    e.preventDefault(); 
-
-    let target = $(this).attr("href"); 
-    console.log(target); 
-
-    let targetPos = $(target).offset().top; 
-    console.log(targetPos); 
-
-    $("html, body").animate({
-        scrollTop : targetPos
-    },1000);
-});
-
-
+}
